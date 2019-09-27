34a57efceb9c5f12f1d67da9a44849031858e93f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-27 00:49:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,7 +21,7 @@ class InteractiveRecord
 
   def initialize(options={})
     options.each do |property, value|
-      self.send('#{property}', value)
+      self.send("#{property}=", value)
     end
   end
 
