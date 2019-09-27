ac6a927047c29a495325544b32212c82a7f3eb15
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-27 01:10:41 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,7 +53,10 @@ class InteractiveRecord
   end
 
   def self.find_by(id)
-    sql = "SELECT * FROM #{self.table_name} WHERE id = ?"
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name} 
+      WHERE id = ?
+    SQL
 
     DB[:conn].execute(sql, id)
   end
